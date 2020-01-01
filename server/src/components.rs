9abcfe84fcc4a76157737325092230032f3831b7
elliosson <elliasss@laposte.net>
2020-01-01 17:31:24 +0100

    Initial: commit Use roguelike tutorial, change map, add tree

---
+++ server/src/components.rs
@@ -0,0 +1,163 @@
+extern crate specs;
+use specs::prelude::*;
+extern crate specs_derive;
+extern crate rltk;
+use rltk::{RGB};
+use serde::{Serialize, Deserialize};
+use specs::saveload::{Marker, ConvertSaveload};
+use specs::error::NoError;
+
+#[derive(Component, ConvertSaveload, Clone)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
+#[derive(Component, ConvertSaveload, Clone)]
+pub struct Renderable {
+    pub glyph: u8,
+    pub fg: RGB,
+    pub bg: RGB,
+    pub render_order : i32
+}
+
+#[derive(Component, Debug, Serialize, Deserialize, Clone)]
+pub struct Player {}
+
+#[derive(Component, ConvertSaveload, Clone)]
+pub struct Viewshed {
+    pub visible_tiles : Vec<rltk::Point>,
+    pub range : i32,
+    pub dirty : bool
+}
+
+#[derive(Component, Debug, Serialize, Deserialize, Clone)]
+pub struct Monster {}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct Name {
+    pub name : String
+}
+
+#[derive(Component, Debug, Serialize, Deserialize, Clone)]
+pub struct BlocksTile {}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct CombatStats {
+    pub max_hp : i32,
+    pub hp : i32,
+    pub defense : i32,
+    pub power : i32
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct WantsToMelee {
+    pub target : Entity
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct SufferDamage {
+    pub amount : i32
+}
+
+#[derive(Component, Debug, Serialize, Deserialize, Clone)]
+pub struct Item {}
+
+#[derive(Component, Debug, Serialize, Deserialize, Clone)]
+pub struct Build {}
+
+#[derive(Component, Debug, Serialize, Deserialize, Clone)]
+pub struct Consumable {}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct Ranged {
+    pub range : i32
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct InflictsDamage {
+    pub damage : i32
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct AreaOfEffect {
+    pub radius : i32
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct Confusion {
+    pub turns : i32
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct ProvidesHealing {
+    pub heal_amount : i32
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct InBackpack {
+    pub owner : Entity
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct WantsToPickupItem {
+    pub collected_by : Entity,
+    pub item : Entity
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct WantsToInteractBuild {
+    pub interacted_by : Entity,
+    pub build : Entity
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct WantsToUseItem {
+    pub item : Entity,
+    pub target : Option<rltk::Point>
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct WantsToDropItem {
+    pub item : Entity
+}
+
+#[derive(Component, Debug, ConvertSaveload, Clone)]
+pub struct WantsToRemoveItem {
+    pub item : Entity
+}
+
+#[derive(PartialEq, Copy, Clone, Serialize, Deserialize)]
+pub enum EquipmentSlot { Melee, Shield }
+
+#[derive(Component, Serialize, Deserialize, Clone)]
+pub struct Equippable {
+    pub slot : EquipmentSlot
+}
+
+#[derive(Component, ConvertSaveload, Clone)]
+pub struct Equipped {
+    pub owner : Entity,
+    pub slot : EquipmentSlot
+}
+
+#[derive(Component, ConvertSaveload, Clone)]
+pub struct MeleePowerBonus {
+    pub power : i32
+}
+
+#[derive(Component, ConvertSaveload, Clone)]
+pub struct DefenseBonus {
+    pub defense : i32
+}
+
+// Serialization helper code. We need to implement ConvertSaveLoad for each type that contains an
+// Entity.
+
+pub struct SerializeMe;
+
+// Special component that exists to help serialize the game data
+#[derive(Component, Serialize, Deserialize, Clone)]
+pub struct SerializationHelper {
+    pub map : super::map::Map
+}
