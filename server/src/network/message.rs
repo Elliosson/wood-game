653b277f45727077589741923e5e8b368048179c
elliosson <elliasss@laposte.net>
2020-03-04 00:07:57 +0100

    Add interact message parsing

--- server/src/network/message.rs
+++ server/src/network/message.rs
@@ -35,6 +35,16 @@ impl Message {
                 "right" => Some(Message::RIGHT(id)),
                 "left" => Some(Message::LEFT(id)),
                 "exit" => Some(Message::Exit(id)),
+                "interact" => {
+                    let interact: Vec<&str> = parts.collect();
+                    let x: i32 = interact[0].parse().unwrap();
+                    let y: i32 = interact[1].parse().unwrap();
+                    let name: String = interact[2].parse().unwrap();
+                    let ent_id: u32 = interact[3].parse().unwrap();
+                    let gen: i32 = interact[4].parse().unwrap();
+
+                    Some(Message::Interact(id, x, y, name, ent_id, gen))
+                }
                 _ => None,
             };
 
