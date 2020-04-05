5639ff1c51028326a17a17944af8654559161977
elliosson <elliasss@laposte.net>
2020-04-05 21:52:58 +0200

    Move rltk system in a new file

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{Console, GameState, Rltk, RGB};
+use rltk::Rltk;
 #[macro_use]
 extern crate specs_derive;
 mod components;
@@ -12,7 +12,7 @@ mod desktop_network;
 
 mod runstate;
 
-use runstate::{player_input, Runstate};
+use runstate::Runstate;
 
 use std::sync::{Arc, Mutex};
 
@@ -21,7 +21,10 @@ use wasm_bindgen::prelude::*;
 
 extern crate specs;
 use specs::prelude::*;
-mod gui;
+pub mod gui;
+
+mod rltk_main;
+pub use rltk_main::*;
 
 #[cfg(target_arch = "wasm32")]
 macro_rules! console_log {
@@ -46,97 +49,8 @@ pub fn consol_print(mes: String) {
     println!("{}", mes);
 }
 
-struct State {
-    pub rectangle: Rect,
-    pub data: Arc<Mutex<Data>>,
-    pub to_send: Arc<Mutex<Vec<String>>>,
-    pub player_info: PlayerInfo,
-    pub runstate: Runstate,
-    pub ecs: World,
-    pub pseudo: String,
-}
-
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut Rltk) {
-        let data_guard = self.data.lock().unwrap();
-
-        match serde_json::from_str(&data_guard.info_string) {
-            Ok(info) => self.player_info = info,
-            Err(_) => {
-                consol_print("unable to deserialize json".to_string());
-            }
-        }
-
-        ctx.cls();
-
-        draw_map(ctx, data_guard.map.clone(), &self.player_info.my_info.pos);
-
-        gui::draw_ui(ctx, &self.player_info);
-
-        self.runstate = player_input(
-            data_guard.my_uid.clone(),
-            self.to_send.clone(),
-            ctx,
-            &self.runstate,
-            &mut self.rectangle,
-            &self.player_info,
-            &mut self.pseudo,
-        );
-
-        for pos in data_guard.characters.iter() {
-            ctx.set(
-                pos.x,
-                pos.y,
-                RGB::named(rltk::WHITE),
-                RGB::named(rltk::BLACK),
-                rltk::to_cp437('X'),
-            );
-        }
-    }
-}
-
-pub struct Rect {
-    pub width: i32,
-    pub height: i32,
-    pub x: i32,
-    pub y: i32,
-}
-
-pub fn draw_rect(ctx: &mut Rltk, rect: &Rect) {
-    for x in rect.x..rect.width + rect.x {
-        for y in rect.y..rect.height + rect.y {
-            ctx.set(
-                x,
-                y,
-                RGB::named(rltk::WHITE),
-                RGB::named(rltk::BLACK),
-                rltk::to_cp437('#'),
-            );
-        }
-    }
-}
-
-pub struct Data {
-    characters: Vec<Point>,
-    my_uid: String,
-    map: Vec<(Point, Renderable)>,
-    info_string: String,
-}
-
-fn draw_map(ctx: &mut Rltk, mut map: Vec<(Point, Renderable)>, my_pos: &Position) {
-    let center_x = 30;
-    let center_y = 30;
-    map.sort_by(|a, b| b.1.render_order.cmp(&a.1.render_order));
-    for (pos, render) in map.iter() {
-        let x = pos.x - my_pos.x + center_x;
-        let y = pos.y - my_pos.y + center_y;
-        if gui::inside_windows(x, y) {
-            ctx.set(x, y, render.fg, render.bg, render.glyph);
-        }
-    }
-}
-
 fn main() {
+    //Shared data between the network and the game system
     let data = Data {
         characters: vec![],
         my_uid: "".to_string(),
@@ -145,6 +59,11 @@ fn main() {
     };
     let protect_data: Arc<Mutex<Data>> = Arc::new(Mutex::new(data));
     let to_send: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
+    lauch_network(protect_data.clone(), to_send.clone());
+    rltk_init(protect_data.clone(), to_send.clone());
+}
+
+pub fn rltk_init(protect_data: Arc<Mutex<Data>>, to_send: Arc<Mutex<Vec<String>>>) {
     let context = Rltk::init_simple8x8(180 as u32, 90 as u32, "Ecosystem simulator", "resources");
     let gs = State {
         rectangle: Rect {
@@ -173,7 +92,6 @@ fn main() {
         pseudo: "".to_string(),
     };
 
-    lauch_network(protect_data.clone(), to_send.clone());
     rltk::main_loop(context, gs);
 }
 
---
+++ client/src/rltk_main.rs
@@ -0,0 +1,128 @@
+use rltk::{Console, GameState, Rltk, RGB};
+
+pub use super::{
+    components::*,
+    gui,
+    runstate::{player_input, Runstate},
+};
+
+use std::sync::{Arc, Mutex};
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+extern crate specs;
+use specs::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+extern "C" {
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[cfg(target_arch = "wasm32")]
+pub fn consol_print(mes: String) {
+    console_log!("{}", mes);
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn consol_print(mes: String) {
+    println!("{}", mes);
+}
+
+pub struct State {
+    pub rectangle: Rect,
+    pub data: Arc<Mutex<Data>>,
+    pub to_send: Arc<Mutex<Vec<String>>>,
+    pub player_info: PlayerInfo,
+    pub runstate: Runstate,
+    pub ecs: World,
+    pub pseudo: String,
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        let data_guard = self.data.lock().unwrap();
+
+        match serde_json::from_str(&data_guard.info_string) {
+            Ok(info) => self.player_info = info,
+            Err(_) => {
+                consol_print("unable to deserialize json".to_string());
+            }
+        }
+
+        ctx.cls();
+
+        draw_map(ctx, data_guard.map.clone(), &self.player_info.my_info.pos);
+
+        gui::draw_ui(ctx, &self.player_info);
+
+        self.runstate = player_input(
+            data_guard.my_uid.clone(),
+            self.to_send.clone(),
+            ctx,
+            &self.runstate,
+            &mut self.rectangle,
+            &self.player_info,
+            &mut self.pseudo,
+        );
+
+        for pos in data_guard.characters.iter() {
+            ctx.set(
+                pos.x,
+                pos.y,
+                RGB::named(rltk::WHITE),
+                RGB::named(rltk::BLACK),
+                rltk::to_cp437('X'),
+            );
+        }
+    }
+}
+
+pub struct Rect {
+    pub width: i32,
+    pub height: i32,
+    pub x: i32,
+    pub y: i32,
+}
+
+pub fn draw_rect(ctx: &mut Rltk, rect: &Rect) {
+    for x in rect.x..rect.width + rect.x {
+        for y in rect.y..rect.height + rect.y {
+            ctx.set(
+                x,
+                y,
+                RGB::named(rltk::WHITE),
+                RGB::named(rltk::BLACK),
+                rltk::to_cp437('#'),
+            );
+        }
+    }
+}
+
+pub struct Data {
+    pub characters: Vec<Point>,
+    pub my_uid: String,
+    pub map: Vec<(Point, Renderable)>,
+    pub info_string: String,
+}
+
+fn draw_map(ctx: &mut Rltk, mut map: Vec<(Point, Renderable)>, my_pos: &Position) {
+    let center_x = 30;
+    let center_y = 30;
+    map.sort_by(|a, b| b.1.render_order.cmp(&a.1.render_order));
+    for (pos, render) in map.iter() {
+        let x = pos.x - my_pos.x + center_x;
+        let y = pos.y - my_pos.y + center_y;
+        if gui::inside_windows(x, y) {
+            ctx.set(x, y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
