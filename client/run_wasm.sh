0d664a192fe4df8667543375caf6dbeea4697609
elliosson <elliasss@laposte.net>
2021-08-25 20:59:12 +0200

    Remove amethyst ressource file and old wasm script

--- client/run_wasm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cargo build --release --target wasm32-unknown-unknown
-
-wasm-bindgen ./target/wasm32-unknown-unknown/release/client.wasm --out-dir webpage/ --no-modules --no-typescript
-
-http-server ./webpage/
